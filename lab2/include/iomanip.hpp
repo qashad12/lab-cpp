408ed0a5d80d1479cc8baca6ab3264dca369e42e
qashad12 <morororz@mail.ru>
2023-01-31 14:37:48 +0300

    lab2: done

--- lab2/include/iomanip.hpp
+++ lab2/include/iomanip.hpp
@@ -9,15 +9,62 @@
 
 #include <iostream>
 
-//{ endm manipulator
+namespace {
+    class __endm {
+        friend std::ostream &operator <<(std::ostream &out, const __endm &) {
+            return out << "[eol]\n";
+        }
+    };
 
-//}
+    class __squares {
+        std::ostream *save;
 
-//{ squares manipulator
+        friend __squares &operator <<(std::ostream &out, __squares &sq) {
+            sq.save = &out;
+            return sq;
+        }
 
-//}
+        template <class value_type>
+        friend std::ostream &operator <<(__squares &sq, const value_type &val) {
+            return *sq.save << '[' << val << ']';
+        }
+    };
 
-//{ add manipulator
+    class __add_base;
+
+    template <class value_type>
+    struct __add_impl {
+        const value_type &save;
+        std::ostream *stream;
+
+        __add_impl(const value_type &val, std::ostream *out)
+            : save(val), stream(out)
+        {}
+
+        friend std::ostream &operator <<(const __add_impl<value_type> &out, const value_type &val) {
+            return *out.stream << (out.save + val);
+        }
+    };
+
+    class __add_base {
+        std::ostream *save;
+
+        friend __add_base &operator <<(std::ostream &out, __add_base &add) {
+            add.save = &out;
+            return add;
+        }
+
+        template <class value_type>
+        friend __add_impl<value_type> operator <<(const __add_base &out, const value_type &val) {
+            return __add_impl<value_type>(val, out.save);
+        }
+    };
+
+}
+
+[[maybe_unused]] inline __endm endm;
+[[maybe_unused]] inline __squares squares;
+[[maybe_unused]] inline __add_base add;
 
 //}
 
